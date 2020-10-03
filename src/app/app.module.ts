c67b9f7dcb9aebb8eaa761e8ba3596149941df9e
Andrew Siegman <blerrnk@gmail.com>
2020-10-02 19:46:02 -0500

    project details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { LimitToPipe } from './limit-to.pipe';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
     LimitToPipe,
+    ProjectDetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +26,7 @@ import { LimitToPipe } from './limit-to.pipe';
     MatTableModule,
     MatSlideToggleModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
